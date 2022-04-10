25323e843513dc805fdea4d66665bd3ef127a1fe
Carina T <31992768+karinature@users.noreply.github.com>
2022-04-10 19:14:46 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/_deps/json-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/carina/CLionProjects/EncryptedKMeans/cmake-build-debug/_deps/json-src
-# Build directory: /home/carina/CLionProjects/EncryptedKMeans/cmake-build-debug/_deps/json-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
